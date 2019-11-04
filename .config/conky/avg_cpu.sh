df45e27c77e064af57d489b0f2845f4da1c60b20
Alex Burka <aburka@exyntechnologies.com>
2019-11-03 20:41:05 -0500

    round cpu speed

--- .config/conky/avg_cpu.sh
+++ .config/conky/avg_cpu.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-awk '/cpu MHz/ {sum+=$NF; num+=1} END {print sum/num/1024}' /proc/cpuinfo
+source $(dirname $0)/utilities.bash
+
+ghz=$(awk '/cpu MHz/ {sum+=$NF; num+=1} END {print sum/num/1024}' /proc/cpuinfo)
+echo $(si_prefix $(calc "$ghz*1024*1024"))Hz
 
